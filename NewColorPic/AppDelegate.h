2115c2fad2455bb320372ad62c3ae7322bf79321
RK905 <mr.rayenk@gmail.com>
2015-09-29 23:16:54 -0400

    Initial Commit

---
+++ NewColorPic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewColorPic
+//
+//  Created by Rayen Kamta on 9/29/15.
+//  Copyright © 2015 Geeksdobyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewColorPic/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NewColorPic
+//
+//  Created by Rayen Kamta on 9/29/15.
+//  Copyright © 2015 Geeksdobyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
